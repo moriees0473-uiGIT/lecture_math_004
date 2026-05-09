17a47a226cb9dbe01f860bfa66e08a9f15c06b80
moriees25 <moriees0473@gmail.com>
2026-05-09 12:05:24 +0900

    init

---
+++ lecture_build.ps1
@@ -0,0 +1,23 @@
+# ０．初期化（テンプレート） ナビゲーションメニュー（目次リスト）
+# npm create slidev@latest
+
+# npm create slidev@latest
+# npm install @slidev/cli@0.49.29
+
+# npx @slidev/cli --version     52.14.2
+
+# .gitフォルダ削除
+# git init
+# git add .
+# git commit -m "init"
+# git remote add origin https://github.com/moriees0473-uiGIT/lecture_math_004.git
+# git branch -M main
+# git push -u origin main
+
+
+npx slidev
+# http://localhost:3030/
+
+# npm run serve
+
+# npm ci --include=optional && npm run build:render
